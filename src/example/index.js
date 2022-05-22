27bbf9e914ab0c7775f4c7d495fd566c122116d3
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2022-05-22 09:55:13 -0400

    added pollForValue

--- src/example/index.js
+++ src/example/index.js
@@ -5,5 +5,8 @@ const EXAMPLE_API = process.env["EXAMPLE_API"];
 
 const jsonService = RestClient(EXAMPLE_API);
 
-const data = await jsonService.get("/todos/1");
+let data = await jsonService.get("/todos/1");
+console.log("LOG:  ~ file: index.js ~ line 12 ~ data", JSON.stringify(data));
+
+data = await jsonService.pollForValue("/todos/1", "title");
 console.log("LOG:  ~ file: index.js ~ line 12 ~ data", JSON.stringify(data));
