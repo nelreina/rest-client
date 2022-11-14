266fd2f66a81b86168804f1f9c5d38751aae0cb2
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2022-11-14 04:02:38 -0400

    test json changed

--- src/example/json.js
+++ src/example/json.js
@@ -9,8 +9,11 @@ try {
     throw new Error("JSON_API is not defined. Please set it in your .env file");
   }
   const jsonService = new RestClient(JSON_API);
-  let data = await jsonService.get("/todos/1");
-  console.log("LOG:  ~ file: json.js ~ line 13 ~ data", JSON.stringify(data));
+  let response = await jsonService.get("/todos/1", false, true);
+  const { data, headers } = response;
+  console.log("LOG:  ~ file: json.js ~ line 14 ~ headers", headers);
+
+  console.log("LOG:  ~ file: json.js ~ line 13 ~ data", data);
 } catch (error) {
   console.error("ERROR: ~ file: index.js ~ line 27 ~ error", error.message);
 }
